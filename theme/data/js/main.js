bf460e2769ee2296a4ebf5070715c8216c84dbbb
superhx <hanxvdovehx@gmail.com>
2015-06-17 20:41:33 +0800

    new blog

--- theme/data/js/main.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("load", function () {
-    new MediumEditor(document.querySelector(".editor"), {buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote']});
-});
